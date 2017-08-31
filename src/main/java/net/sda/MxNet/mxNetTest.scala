35851af9ce9e29e825b1188f43e1f7b9c3882052
Afshin Sadeghi <sadeghi@cs.uni-bonn.de>
2017-08-31 10:32:43 +0200

    testing makig big matrix

---
+++ src/main/java/net/sda/MxNet/mxNetTest.scala
@@ -0,0 +1,16 @@
+package net.sda.MxNet;
+
+import ml.dmlc.mxnet._
+import ml.dmlc.mxnet.optimizer.SGD
+
+
+object mxNetTest {
+    
+def main(args: Array[String]) = { 
+val a = NDArray.zeros(100, 50)
+val aux = NDArray.zeros(11886, 916, 11285)
+println("made a big matrix!")
+System.exit(1)
+ }
+}
+
